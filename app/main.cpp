9c5941bad69240d4b366bd3b71e56c1058f5757d
Alexey <aoklyunin@gmail.com>
2023-03-09 13:04:03 +0300

    init

---
+++ app/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "patterns.h"
+
+int main() {
+    std::cout << sum(10, 20) << std::endl;
+    return 0;
+}
---
+++ patterns/tests/sumTest.cpp
@@ -0,0 +1,6 @@
+#include <cassert>
+#include "patterns.h"
+
+int main() {
+    assert(sum(10, 20) == 30);
+}
